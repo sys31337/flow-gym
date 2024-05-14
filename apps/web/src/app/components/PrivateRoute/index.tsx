5a05a735c6fd64472bf882660413697b582a10bb
MMohamedRida <mebdoua.m@bessapromotion.com>
2024-05-14 16:33:19 +0100

    d

--- apps/web/src/app/components/PrivateRoute/index.tsx
+++ apps/web/src/app/components/PrivateRoute/index.tsx
@@ -15,10 +15,8 @@ const PrivateRoute = (WrappedComponent: React.FC<Any>) => {
     const pathname = usePathname();
     const { state: { user }, loading: authLoading } = useAuth();
     useEffect(() => {
-      console.log(user);
       if (!authLoading) {
         if (!user) {
-          console.log('here');
           router.replace('/signin');
         } else {
           setLoading(false);
